03516fe10f9b5435b825ada06c18aa149055603d
gumshoe_media_inc <gumshoe.media.inc@gmail.com>
2020-09-05 19:20:55 -0600

    fixed setup.py Operating System for PyPi pkg upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
-  'Operating System :: OS INDEPENDENT',
+  'Operating System :: Operating System :: OS Independent',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
